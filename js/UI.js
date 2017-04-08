e9f2ce1c0227c9cc4c5fb46d83a9a6ed0ebfb6c3
DesertJay <desertjaybirdie@gmail.com>
2017-04-07 18:56:24 -0700

    Draw Serve Randomization
    It randomizes racket color, text, and blue player spawn for the serve,
    but red player still serves once game starts.

--- js/UI.js
+++ js/UI.js
@@ -6,77 +6,81 @@ function drawUI(){
 */
 var spinFrame = 0;
 var spinStepsPerAnimFrame = 2;
-var spinFrameTimer=2;
-var numFullSpins=2;
+var spinFrameTimer = 2;
+var numFullSpins = 2;
 var spinAnimationFrames;
+var endAnimation = false;
 
-drawNow=false;
+drawNow = false;
 
-function drawStaminaBar(){
-	//draw bar background
-	colorRect(0,canvas.height-20, 100, 20, 'LightGray');
-	//draw current stamina
-	colorRect(0,canvas.height-20, PlayerClass.sprintStamina * 5, 20, 'red');
-	//draw label
-	colorText('STAMINA', 100,canvas.height-30, 'white');
+function drawStaminaBar() {
+    //draw bar background
+    colorRect(0, canvas.height - 20, 100, 20, 'LightGray');
+    //draw current stamina
+    colorRect(0, canvas.height - 20, PlayerClass.sprintStamina * 5, 20, 'red');
+    //draw label
+    colorText('STAMINA', 100, canvas.height - 30, 'white');
 }
 
-function rightToServe(){
-	const SERVE_W=330;
-	const SERVE_H=365;
-  var endAnimation;
-  var drawLocation = perspectiveLocation(COURT_W/2,COURT_L*0.2,0);
+function rightToServe() {
+    const SERVE_W = 330;
+    const SERVE_H = 365;
+    var drawLocation = perspectiveLocation(COURT_W / 2, COURT_L * 0.2, 0);
 
-  if(numFullSpins>0){
-  var spinAnimationFrames = serve_spin.width/SERVE_W;
-  } else {
-    drawNow=true;
-    if(Math.random>0.5){
-    spinAnimationFrames=0
-  } else {
-    spinAnimationFrames=5
-  }
-}
-
-  if (spinFrameTimer-- < 0 && drawNow==false) {
-      spinFrameTimer = spinStepsPerAnimFrame;
-      spinFrame++;
-  }
-
-  if (spinFrame >= spinAnimationFrames) {
-      spinFrame = 0;
-      numFullSpins--;
-  }
-  if(drawNow){
-    rightToServeOutcome();
-  } else {
-  var titleText = "Drawing Right To Serve, please hold";
-  canvasContext.fillStyle = "grey";
-  canvasContext.textAlign = "center";
-  canvasContext.font = "bold 15px verdana";
-  canvasContext.fillText((titleText), canvas.width / 2, 490);
-    }
-  drawAtBaseSheetSprite(serve_spin, spinFrame, drawLocation.x, drawLocation.y,SERVE_W,SERVE_H);
-}
-
-function rightToServeOutcome(){
-  var drawLocationPlayer = perspectiveLocation(COURT_W*0.3,COURT_L*0.8,0);
-  var subText = "Press Enter to continue";
-  canvasContext.fillStyle = "black";
-  canvasContext.textAlign = "center";
-  canvasContext.font = "bold 15px verdana";
-  canvasContext.fillText((subText), canvas.width / 2, 515);
-
-
-  if(spinAnimationFrames==5){
-    drawAtBaseSheetSprite(p1_standing,0, drawLocationPlayer.x, drawLocationPlayer.y,PLAYER_W,PLAYER_H);
-    var titleText = "Blue Player Has  Right To Serve !";
-    canvasContext.fillStyle = "blue";
+    if (numFullSpins > 0) {
+        spinAnimationFrames = serve_spin.width / SERVE_W;
     } else {
-      var titleText = "Red Player Has Right To Serve !";
-      canvasContext.fillStyle = "red";
-  }
+        drawNow = true;
+        if (Math.random() >= 0.5) {
+            spinAnimationFrames = 0;
+        } else {
+            spinAnimationFrames = 5;
+        }
+    }
 
-  canvasContext.font = "bold 20px verdana";
-  canvasContext.fillText((titleText), canvas.width / 2, 485);
+    if (spinFrameTimer-- < 0 && drawNow == false) {
+        spinFrameTimer = spinStepsPerAnimFrame;
+        spinFrame++;
+    }
+    if (!drawNow) {
+        if (spinFrame >= spinAnimationFrames) {
+            spinFrame = 0;
+            numFullSpins--;
+        }
+        var titleText = "Drawing Right To Serve, please hold";
+        canvasContext.fillStyle = "grey";
+        canvasContext.textAlign = "center";
+        canvasContext.font = "bold 15px verdana";
+        canvasContext.fillText((titleText), canvas.width / 2, 490);
+    }
+    else {
+        if (!endAnimation) {
+            spinFrame = spinAnimationFrames;
+            endAnimation = true;
+        }
+        rightToServeOutcome();
+    }
+    drawAtBaseSheetSprite(serve_spin, spinFrame, drawLocation.x, drawLocation.y, SERVE_W, SERVE_H);
+}
+
+function rightToServeOutcome() {
+    var drawLocationPlayer = perspectiveLocation(COURT_W * 0.3, COURT_L * 0.8, 0);
+    var subText = "Press Enter to continue";
+    canvasContext.fillStyle = "black";
+    canvasContext.textAlign = "center";
+    canvasContext.font = "bold 15px verdana";
+    canvasContext.fillText((subText), canvas.width / 2, 515);
+
+
+    if (spinFrame == 5) {
+        drawAtBaseSheetSprite(p1_standing, 0, drawLocationPlayer.x, drawLocationPlayer.y, PLAYER_W, PLAYER_H);
+        var titleText = "Blue Player Has  Right To Serve !";
+        canvasContext.fillStyle = "blue";
+    } else {
+        var titleText = "Red Player Has Right To Serve !";
+        canvasContext.fillStyle = "red";
+    }
+
+    canvasContext.font = "bold 20px verdana";
+    canvasContext.fillText((titleText), canvas.width / 2, 485);
 }
